a6a926b3c2c829507f8315d8b290702bf6eff1b4
Marcin <mboczkowski2@gmail.com>
2018-10-16 21:08:09 +0200

    V1.1 adding styles

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-let btn = document.getElementsByClassName("btnNumber");
-btn = [...btn]
-let inp = document.getElementById("inp");
-let reset = document.getElementById("btnReset");
-let calc = document.getElementById("btnCalc");
+let btn = document.getElementsByClassName("btnCalc");
+btn = [...btn];
+let score = document.getElementById("score");
+let reset = document.getElementById("reset");
+let calc = document.getElementById("calc");
 let lastNumbers = document.getElementById("lastNumbers");
-let lastCalc = document.getElementById("lastCalc");
+let historyList = document.getElementById("historyList");
 
 reset.addEventListener("click", () => {
-    inp.value = "";
+    score.textContent = "";
+    lastNumbers.textContent = "";
 });
 
-
 btn.forEach((e) => {
-    e.addEventListener("click", function () {
-        console.log(e.getAttribute("data-num"));
-        inp.value += e.getAttribute("data-num");
+
+    e.addEventListener("click", () => {
+        score.textContent += e.getAttribute("data-num");
     });
 });
 
-calc.addEventListener("click", () => {
-    lastNumbers.textContent = inp.value;
-    inp.value = eval(inp.value);
-    lastCalc.textContent = ` = ${eval(inp.value)}`;
 
+calc.addEventListener("click", () => {
+
+    let timeNow = new Date().toLocaleTimeString();
+    let result = eval(score.textContent)
+    let li = document.createElement("li");
+    lastNumbers.textContent = score.textContent;
+    historyList.appendChild(li);
+    li.classList.add("historyRow");
+
+    li.innerHTML = `<p class="time">${timeNow}</p><p class="numbersHistory">${score.textContent}<span class="resultHistory"> = ${result}</span><span></span></p>`;
+    score.textContent = result.toFixed(2);
 });
